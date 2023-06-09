218438ed3bc3ee2a83bd90a5f2ee461633ad8809
JackFrost24 <93308977+JackFrost24@users.noreply.github.com>
2023-06-09 20:38:41 +0800

    Update main.py
    Menambahkan potongan code untuk mengirimkan hasil ke cloud storage

--- CC/api_ml/main.py
+++ CC/api_ml/main.py
@@ -6,8 +6,17 @@ from io import BytesIO
 from tensorflow.keras.utils import img_to_array #pip install tensorflow keras
 import numpy as np #pip install numpy
 from tensorflow.keras.models import load_model
+from google.cloud import storage 
 
 app = Flask(__name__)
+
+
+# create storage client
+storage_client = storage.Client()
+
+# define bucket details
+bucket_name = 'cekmate_data_deteksi'
+bucket = storage_client.bucket(bucket_name)
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -40,6 +49,15 @@ def predict():
     # use json module to preserve key order
     json_response = json.dumps(response, sort_keys=False)
     return json_response
+
+     # upload result to cloud storage
+    result_blob = bucket.blob('result.json')
+    result_blob.upload_from_string(result_json, content_type='application/json')
+    result_blob.acl.all().grant_read()
+    result_blob.acl.save()
+
+    # return prediction result
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
