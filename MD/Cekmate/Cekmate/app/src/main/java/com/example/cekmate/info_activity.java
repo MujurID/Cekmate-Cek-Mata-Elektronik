535cae470f3d122c80f2e3ff26392eaaab5bf3d8
LeonardoAurin <129376485+LeonardoAurin@users.noreply.github.com>
2023-06-09 17:54:04 +0800

    Add files via upload

---
+++ MD/Cekmate/Cekmate/app/src/main/java/com/example/cekmate/info_activity.java
@@ -0,0 +1,88 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		info
+	 *	@date 		Friday 09th of June 2023 09:24:46 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.cekmate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class info_activity extends Activity {
+
+	
+	private View _bg__info_ek2;
+	private View rectangle_1;
+	private TextView informasi;
+	private ImageView history;
+	private ImageView arrow_forward;
+	private View rectangle_25;
+	private ImageView ellipse_1;
+	private TextView leukokoria;
+	private View ellipse_1_ek1;
+	private View rectangle_9;
+	private View ellipse_1_ek2;
+	private View rectangle_9_ek1;
+	private View ellipse_1_ek3;
+	private View rectangle_9_ek2;
+	private View ellipse_1_ek4;
+	private View line_3;
+	private View line_4;
+	private View rectangle_9_ek3;
+	private View rectangle_10;
+	private ImageView rectangle_1_ek1;
+	private TextView leukokoria_ek1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.info);
+
+		
+		_bg__info_ek2 = (View) findViewById(R.id._bg__info_ek2);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		informasi = (TextView) findViewById(R.id.informasi);
+		history = (ImageView) findViewById(R.id.history);
+		arrow_forward = (ImageView) findViewById(R.id.arrow_forward);
+		rectangle_25 = (View) findViewById(R.id.rectangle_25);
+		ellipse_1 = (ImageView) findViewById(R.id.ellipse_1);
+		leukokoria = (TextView) findViewById(R.id.leukokoria);
+		ellipse_1_ek1 = (View) findViewById(R.id.ellipse_1_ek1);
+		rectangle_9 = (View) findViewById(R.id.rectangle_9);
+		ellipse_1_ek2 = (View) findViewById(R.id.ellipse_1_ek2);
+		rectangle_9_ek1 = (View) findViewById(R.id.rectangle_9_ek1);
+		ellipse_1_ek3 = (View) findViewById(R.id.ellipse_1_ek3);
+		rectangle_9_ek2 = (View) findViewById(R.id.rectangle_9_ek2);
+		ellipse_1_ek4 = (View) findViewById(R.id.ellipse_1_ek4);
+		line_3 = (View) findViewById(R.id.line_3);
+		line_4 = (View) findViewById(R.id.line_4);
+		rectangle_9_ek3 = (View) findViewById(R.id.rectangle_9_ek3);
+		rectangle_10 = (View) findViewById(R.id.rectangle_10);
+		rectangle_1_ek1 = (ImageView) findViewById(R.id.rectangle_1_ek1);
+		leukokoria_ek1 = (TextView) findViewById(R.id.leukokoria_ek1);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
